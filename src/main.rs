11d87ea6ae15b122663fdc383376d9bf9d671b15
Antoni Stevent <itzmjauz@gmail.com>
2021-06-17 00:44:39 +0200

    setup testing env

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,60 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 mod vga_buffer;
+mod serial;
 
 use core::panic::PanicInfo; // panic handler import
 
+// Function called at panic
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
+// Testing framework
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    // exit qemu
+    exit_qemu(QemuExitCode::Success);
+}
+// Qemu exit  code
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+// Qemu exit function
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
 
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // entry point of system
-
     println!("Hello World! : #{}", 1);
+
+    #[cfg(test)]
+    test_main();
     loop {}
+}
+
+//testcases
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
