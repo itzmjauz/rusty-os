285dfb16807996c3f081741ed4a716f379e1e264
Antoni Stevent <itzmjauz@gmail.com>
2021-06-18 23:07:12 +0200

    this stuff just wont work

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,24 @@
 #![no_std] // do not load the standard library
 #![no_main] // main is not the entry point
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-#![feature(custom_test_frameworks)]
-#![test_runner(rusty_os::test_runner)]
-#![reexport_test_harness_main = "test_main"]
-
-use core::panic::PanicInfo; // panic handler import
-use rusty_os::println;
-
-// Function called at panic
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    rusty_os::test_panic_handler(info)
-}
 
 use bootloader::{entry_point, BootInfo};
+use core::panic::PanicInfo; // panic handler import
+
+// Function called at panic
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    loop {}
+}
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
-    println!("Hello World! : #{}", 1);
+//    println!("Hello World! : #{}", 1);
+    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
+        for byte in framebuffer.buffer_mut() {
+            *byte = 0x90;
+        }
+    }
     
-    #[cfg(test)]
-    test_main();
     loop {}
 }
